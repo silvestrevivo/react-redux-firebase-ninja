9bfac7dc8581edadcf3b1a03ec3fa33de1155d05
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-11 22:13:19 +0100

    adding author on dynamic way on redux action creator

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -6,7 +6,7 @@ const ProjectSummary = ({ project }) => {
       <div className="card-content grey-text text-darken-3">
         <span className="card-title">{project.title}</span>
         <p>{project.content}</p>
-        <p>Posted by Silvestre</p>
+        <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
         <p className="grey-text">3rd okt 2018</p>
       </div>
     </div>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,14 +2,16 @@ export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // initializing functions to reference firebase
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     // this below is asyncronous
     firestore
       .collection("projects")
       .add({
         ...project,
-        authorFirstName: "Silvestre",
-        authorLastName: "Vivo",
-        authorId: 1234,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date()
       })
       .then(() => {
